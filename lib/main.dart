cbfd5d92ec7ce5dc287a98a9dbf63891b488e4e7
unknown <hakkisekerci@gmail.com>
2021-12-27 21:29:11 +0300

    onGenerateRoute

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -12,12 +12,45 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       // ignore: deprecated_member_use
       theme: ThemeData(accentColor: Colors.grey),
-      home: HomePage(),
+      //home: HomePage(),
+      initialRoute: HomePage.routeName,
+      routes: {
+        HomePage.routeName: (context) => HomePage(),
+        RoutePink.routeName: (context) => RoutePink(
+              insan: Insan(ad: "Ali", yas: 25),
+              metin: '',
+            ),
+        RouteGreen.routeName: (context) => RouteGreen(),
+        RouteGrey.routName: (context) => RouteGrey(),
+      },
+      onGenerateRoute: (settings) {
+        // if (settings.name == RoutePink.routeName) {
+        //   return MaterialPageRoute(
+        //       builder: (context) =>
+        //           RoutePink(metin: "Musa", insan: Insan(ad: "West", yas: 41)));
+        // }
+        // return null;
+
+       
+      },
     );
   }
 }
 
+class Kullanici {
+  String ad;
+  int yas;
+  String adres;
+  Kullanici({
+    required this.ad,
+    required this.yas,
+    required this.adres,
+  });
+}
+
 class HomePage extends StatelessWidget {
+  Kullanici kullanici_1 = Kullanici(ad: "Semih", yas: 40, adres: "Merkez");
+  static String routeName = "/";
   Insan insan1 = Insan(ad: "Kahraman", yas: 32);
   @override
   Widget build(BuildContext context) {
@@ -35,14 +68,8 @@ class HomePage extends StatelessWidget {
             ElevatedButton(
                 child: Text("Route Pink Git", style: TextStyle(fontSize: 20)),
                 onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => RoutePink(
-                              insan: insan1,
-                              metin: "Parola :Kara Kış",
-                            )),
-                  );
+                  Navigator.pushNamed(context, RoutePink.routeName,
+                      arguments: kullanici_1);
                 })
           ],
         ),
@@ -52,6 +79,8 @@ class HomePage extends StatelessWidget {
 }
 
 class RoutePink extends StatelessWidget {
+  //Kullanici yerelKullanici = ModalRoute.of(context).settings.arguments;
+  static String routeName = "/routePink";
   String metin;
   Insan insan;
   RoutePink({
@@ -62,11 +91,12 @@ class RoutePink extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+//Kullanici yerelKullanici = ModalRoute.of(context).settings.arguments();
     return Scaffold(
       backgroundColor: Colors.pink,
       appBar: AppBar(
         title: Text(
-          "Route Pink",
+          "Kullanici adi",
           style: TextStyle(fontSize: 20),
         ),
       ),
@@ -81,8 +111,7 @@ class RoutePink extends StatelessWidget {
             ElevatedButton(
                 child: Text("Green Route Git", style: TextStyle(fontSize: 20)),
                 onPressed: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => RouteGreen()));
+                  Navigator.pushNamed(context, RouteGreen.routeName);
                 }),
             ElevatedButton(
                 child: Text("GeriDön", style: TextStyle(fontSize: 20)),
@@ -97,6 +126,7 @@ class RoutePink extends StatelessWidget {
 }
 
 class RouteGreen extends StatelessWidget {
+  static String routeName = "/routeGrey";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -112,10 +142,7 @@ class RouteGreen extends StatelessWidget {
             ElevatedButton(
               child: Text("Route Grey Git", style: TextStyle(fontSize: 20)),
               onPressed: () {
-                Navigator.pushAndRemoveUntil(
-                    context,
-                    MaterialPageRoute(builder: (context) => RouteGrey()),
-                    (route) => false);
+                Navigator.pushNamed(context, RouteGrey.routName);
               },
             ),
             ElevatedButton(
@@ -131,6 +158,7 @@ class RouteGreen extends StatelessWidget {
 }
 
 class RouteGrey extends StatelessWidget {
+  static String routName = "/routeGreey";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -145,11 +173,13 @@ class RouteGrey extends StatelessWidget {
             Text("Şuan RouteGrey en üstte", style: TextStyle(fontSize: 20)),
             ElevatedButton(
                 child: Text("Son....", style: TextStyle(fontSize: 20)),
-                onPressed: () {}),
+                onPressed: () {
+                  Navigator.pushNamed(context, HomePage.routeName);
+                }),
             ElevatedButton(
                 child: Text("Geri Dön", style: TextStyle(fontSize: 20)),
                 onPressed: () {
-                  Navigator.popUntil(context, (route) => route.isFirst);
+                  Navigator.pop(context);
                 })
           ],
         ),
---
+++ lib/map_examples.dart
@@ -0,0 +1,10 @@
+void main() {
+  var adresler = {
+    'NATA': 'SEPTIMO MIAU nº',
+    'GUEV': 'SARGENTO GARCIA NOBLEJAS nº 8',
+    'GAR': 'MIGUEL DE CERVANTES nº 37'
+  };
+
+  print(adresler['NATA']);
+  print(adresler);
+}
